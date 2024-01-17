32510d01ab672924a9a6d3c9e78ac61ca87e95c7
Krzysztof Serafin <serakrzysztof@gmail.com>
2024-01-17 22:14:37 +0100

    calculator+simplyuser+testy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,25 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania!");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("Forum User :test OK");
+        } else {
+            System.out.println("Forum User :^XError!");
+        }
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(10, 20);
+        System.out.println("Add Test 10+20 " + (addResult == 30 ? "Test OK" : "Error"));
+
+        int subtractResult = calculator.subtract(10, 20);
+        System.out.println("Subtract Test 10-20 " + (subtractResult == -10 ? "Test OK" : "Error"));
     }
 }
--- kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
@@ -1,2 +1,12 @@
-package com.kodilla.testing.user;public class SimpleUser {
+package com.kodilla.testing.user;
+
+public class SimpleUser {
+    private String username;
+
+    public SimpleUser(String username) {
+        this.username = username;
+    }
+    public  String getUsername() {
+        return username;
+    }
 }
