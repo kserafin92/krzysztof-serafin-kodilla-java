e8c40021265401adfd3c49f0988a6861a091bb57
Krzysztof Serafin <serakrzysztof@gmail.com>
2024-01-29 23:16:41 +0100

    intStream ver2

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,10 +1,19 @@
 package com.kodilla.stream.array;
 
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
     static double getAverage(int[] numbers) {
+        System.out.println("Kolejne elementy tablicy:");
+        IntStream.range(0, numbers.length)
+                .map(i -> numbers[i])
+                .peek(element -> System.out.print(element))
+                .count();
 
+        System.out.println("Kolejne elementy tablicy:");
+        Arrays.stream(numbers)
+                .forEach(element -> System.out.print(element + " "));
 
         double average = IntStream.range(0, numbers.length)
                 .mapToDouble(i -> numbers[i])
