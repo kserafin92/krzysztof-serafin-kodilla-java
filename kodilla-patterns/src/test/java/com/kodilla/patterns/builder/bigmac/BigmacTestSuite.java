e8d4f45ed088fdf8a6006d955192ed2a681fc60a
Krzysztof Serafin <serakrzysztof@gmail.com>
2024-03-10 14:39:26 +0100

    14.5.0

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,27 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import com.kodilla.patterns.builder.pizza.Pizza;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BigmacTestSuite {
+
+    @Test
+    void testBigmacNew() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .ingredient("Onion")
+                .bun("XYZ")
+                .sauce("barbecue")
+                .burgers(2)
+                .ingredient("bacon")
+                .ingredient("cheese")
+                .build();
+        System.out.println(bigmac);
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+        //Then
+        assertEquals(3, howManyIngredients);
+    }
+}
